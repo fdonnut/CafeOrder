069d9ad39ca0dca8481446ec1193c06b9579b76e
fdonnut <36168787+fdonnut@users.noreply.github.com>
2022-11-29 14:06:35 +0300

    Second screen logic

--- app/src/main/java/com/donnut/cafeorder/MakeOrderActivity.java
+++ app/src/main/java/com/donnut/cafeorder/MakeOrderActivity.java
@@ -5,23 +5,124 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 public class MakeOrderActivity extends AppCompatActivity {
+
+    private static final String EXTRA_USER_NAME = "userName";
+
+    private TextView textViewGreetings;
+    private TextView textViewAdditives;
+
+    private RadioGroup radioGroupDrinks;
+    private RadioButton radioButtonTea;
+    private RadioButton radioButtonCoffee;
+
+    private CheckBox checkboxSugar;
+    private CheckBox checkboxMilk;
+    private CheckBox checkboxLemon;
+
+    private Spinner spinnerTea;
+    private Spinner spinnerCoffee;
+
+    private Button buttonMakeOrder;
+
+    private String userName;
+    private String drink;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_make_order);
+        initViews();
+        setupUserName();
 
-        TextView textViewGreetings = findViewById(R.id.textViewGreetings);
-        Intent intent = getIntent();
-        String name = intent.getStringExtra("name");
+        radioGroupDrinks.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int id) {
+                if (id == radioButtonTea.getId()) {
+                    onUserChoseTea();
+                } else if (id == radioButtonCoffee.getId()) {
+                    onUserChoseCoffee();
+                }
+            }
+        });
+        radioButtonTea.setChecked(true);
+
+        buttonMakeOrder.setOnClickListener(view -> {
+            onUserMadeOrder();
+        });
+    }
+
+    private void onUserMadeOrder() {
+        ArrayList<String> additives = new ArrayList<>();
+        if (checkboxSugar.isChecked()) {
+            additives.add(checkboxSugar.getText().toString());
+        }
+        if (checkboxMilk.isChecked()) {
+            additives.add(checkboxMilk.getText().toString());
+        }
+        if (radioButtonTea.isChecked() && checkboxLemon.isChecked()) {
+            additives.add(checkboxLemon.getText().toString());
+        }
+
+        String drinkType = "";
+        if (radioButtonTea.isChecked()) {
+            drinkType = spinnerTea.getSelectedItem().toString();
+        } else if (radioButtonCoffee.isChecked()) {
+            drinkType = spinnerCoffee.getSelectedItem().toString();
+        }
+        Intent intent = OrderDetailActivity.newIntent(this, userName, drink, drinkType, additives.toString());
+        startActivity(intent);
+    }
+
+    private void onUserChoseTea() {
+        drink = getString(R.string.tea);
+        textViewAdditives.setText(getString(R.string.additives, drink));
+        checkboxLemon.setVisibility(View.VISIBLE);
+        spinnerTea.setVisibility(View.VISIBLE);
+        spinnerCoffee.setVisibility(View.INVISIBLE);
+    }
+
+    private void onUserChoseCoffee() {
+        drink = getString(R.string.coffee);
+        textViewAdditives.setText(getString(R.string.additives, drink));
+        checkboxLemon.setVisibility(View.INVISIBLE);
+        spinnerTea.setVisibility(View.INVISIBLE);
+        spinnerCoffee.setVisibility(View.VISIBLE);
     }
 
     public static Intent newIntent(Context context, String userName) {
         Intent intent = new Intent(context, MakeOrderActivity.class);
-        intent.putExtra("userName", userName);
+        intent.putExtra(EXTRA_USER_NAME, userName);
         return intent;
+    }
+
+    private void initViews() {
+        textViewGreetings = findViewById(R.id.textViewGreetings);
+        textViewAdditives = findViewById(R.id.textViewAdditives);
+        radioGroupDrinks = findViewById(R.id.radioGroupDrinks);
+        radioButtonTea = findViewById(R.id.radioButtonTea);
+        radioButtonCoffee = findViewById(R.id.radioButtonCoffee);
+        checkboxSugar = findViewById(R.id.checkboxSugar);
+        checkboxMilk = findViewById(R.id.checkboxMilk);
+        checkboxLemon = findViewById(R.id.checkboxLemon);
+        spinnerTea = findViewById(R.id.spinnerTea);
+        spinnerCoffee = findViewById(R.id.spinnerCoffee);
+        buttonMakeOrder = findViewById(R.id.buttonMakeOrder);
+    }
+
+    private void setupUserName() {
+        userName = getIntent().getStringExtra(EXTRA_USER_NAME);
+        String greetings = getString(R.string.greetings, userName);
+        textViewGreetings.setText(greetings);
     }
 }
